7d3d0470647cd0eaa5afa17614b9090410fc4ef8
Nico Frieß <nico.friess@eoda.de>
2024-01-10 11:42:04 +0100

    update example

--- examples/basemap.R
+++ examples/basemap.R
@@ -4,4 +4,4 @@ test <- mapOptions(center = c(-123.1256, 49.24658),
                    style = basemaps$carto$dark_matter,
                    test = NULL)
 maplibre(test, zoom = 12) |>
-  addControl("NavigationControl", "top-left", showCompass=F)
+  add_control("NavigationControl", "top-left", showCompass=F)
