f66b927daccc0fc41e59e963f551a7e98a29344d
Nico Frieß <nico.friess@eoda.de>
2024-01-10 11:40:53 +0100

    add add_call utility function

---
+++ R/add_call.R
@@ -0,0 +1,17 @@
+#' Title
+#'
+#' @param .map
+#' @param .name
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+add_call <- function(.map, .name, ...){
+  pos <- length(.map$x$calls) + 1
+  .map$x$calls[[pos]] <- list(
+    .name, list(...) |> purrr::compact()
+  )
+  return(.map)
+}
