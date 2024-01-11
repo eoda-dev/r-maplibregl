886db733f8f78d704c29f8087b1c73db1832877c
Stefan Kuethe <stefan.kuethe@eoda.de>
2024-01-11 10:23:56 +0100

    Remove dead code

--- R/utils.R
+++ R/utils.R
@@ -5,11 +5,8 @@ set_maplibre_class <- function(.obj, class_name) {
 
 rdantic <- function(.obj, types, test = 1L) {
   for (i in 1:length(.obj)) {
-    # print(.obj[[i]])
-    # print(types[[i]])
     type_check <- types[[i]]
     value <- .obj[[i]]
-    # stopifnot(type_check(value))
     if (xor(!type_check(value), is.null(value))) {
       stop(value, " is not ", deparse(substitute(type_check)) , call.=FALSE)
     }
