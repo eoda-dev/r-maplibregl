f96e40fef6613cd8c22c0bd06d1288bfbf6bd454
Nico Frieß <nico.friess@eoda.de>
2024-10-03 21:30:51 +0200

    add LayerType

--- R/layer.R
+++ R/layer.R
@@ -1,6 +1,6 @@
 #' Create a layer
 #'
-#' @param type
+#' @param type A layer type
 #' @param id
 #' @param source
 #' @param paint
@@ -10,6 +10,13 @@
 #'
 #' @export
 Layer <- function(type, id, source = NULL, paint = NULL, layout = NULL, ...) {
+  if(!is.element(type, LayerType)){
+    stop(
+      glue::glue("type needs to be one of ",
+                 glue::glue_collapse(unlist(maplibre::LayerType), sep = " ")
+      )
+    )
+  }
   types <- list(type = is.character, id = is.character, source = is.list, paint = is.list, layout = is.list)
   c(rdantic(
     list(
@@ -24,7 +31,34 @@ Layer <- function(type, id, source = NULL, paint = NULL, layout = NULL, ...) {
     set_maplibre_class("MapLibreLayer")
 }
 
-
+#' Rendering types of layers
+#' @details
+#' Rendering types of layers
+#' \itemize{
+#'   \item \strong{CIRCLE}: A filled circle.
+#'   \item \strong{FILL}: A filled polygon with an optional stroked border.
+#'   \item \strong{FILL_EXTRUSION}: An extruded polygon.
+#'   \item \strong{LINE}: A stroked line.
+#'   \item \strong{SYMBOL}: An icon or a text label.
+#'   \item \strong{RASTER}: Raster map textures such as satellite imagery.
+#'   \item \strong{HEATMAP}: A heatmap.
+#'   \item \strong{HILLSHADE}: A Client-side hillshading visualization based on DEM data.
+#'   \item \strong{BACKGROUND}: A background color or pattern.
+#' }
+#'
+#' @export
+#'
+LayerType <- list(
+  CIRCLE = "circle",
+  FILL = "fill",
+  FILL_EXTRUSION = "fill-extrusion",
+  LINE = "line",
+  SYMBOL = "symbol",
+  RASTER = "raster",
+  HEATMAP = "heatmap",
+  HILLSHADE = "hillshade",
+  BACKGROUND = "background"
+)
 #' Add a layer to map
 #'
 #' @param .map
