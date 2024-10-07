39cacf44f3bd1e597ce1a5e7c201616744096d9c
Nico Frieß <nico.friess@posteo.de>
2024-10-07 19:35:40 +0200

    styler

--- tests/testthat/test-maplibre.R
+++ tests/testthat/test-maplibre.R
@@ -9,6 +9,6 @@ test_that("base map", {
 
   expect_equal(m$x$mapOptions, list(style = "https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json"))
   expect_equal(m0$x$mapOptions, list(style = "https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json"))
-  expect_s3_class(m, c("maplibre","htmlwidget"))
-  expect_s3_class(m0, c("maplibre","htmlwidget"))
+  expect_s3_class(m, c("maplibre", "htmlwidget"))
+  expect_s3_class(m0, c("maplibre", "htmlwidget"))
 })
